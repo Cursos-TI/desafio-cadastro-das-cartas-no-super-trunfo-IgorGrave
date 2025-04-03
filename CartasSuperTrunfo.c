82b49c1fa3a110e60f5aeb2461b7aff48c538892
IgorGrave <igorgrave077@gmail.com>
2025-04-03 23:10:52 +0000

    codigo pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,14 +3,14 @@
 int main() {
     
     //declaração das variaveis carta 1 e carta 2
-    char estado, estado1;
-    char codigo, codigo1;
-    int numerocod, numerocod1;
-    char NomeCidade[20], NomeCidade1[20];
-    int populacao, populacao1;
-    float areaKm, areaKm1;
-    float pib, pib1;
-    int pontostur, pontostur1;
+    char estado, estado2;
+    char codigo, codigo2;
+    int numerocod, numerocod2;
+    char NomeCidade[20], NomeCidade2[20];
+    int populacao, populacao2;
+    float areaKm, areaKm2;
+    float pib, pib2;
+    int pontostur, pontostur2;
 
     //leitura de entrada da carta 1
     printf("Super Trunfo\n");
@@ -39,25 +39,25 @@ int main() {
 
     //leitura de entrada da carta 2
     printf("\nSelecione os dados da sua carta 2\n \nEstado: \n");
-    scanf(" %c", &estado1);
+    scanf(" %c", &estado2);
 
     printf("codigo da carta: \n");
-    scanf(" %c%d", &codigo1, &numerocod1);
+    scanf(" %c%d", &codigo2, &numerocod2);
 
     printf("nome da cidade: \n");
-    scanf("%s", NomeCidade1);
+    scanf("%s", NomeCidade2);
 
     printf("População: \n");
-    scanf("%d", &populacao1);
+    scanf("%d", &populacao2);
 
     printf("Área (em km2): \n");
-    scanf("%f", &areaKm1);
+    scanf("%f", &areaKm2);
 
     printf("PIB: \n");
-    scanf("%f", &pib1);
+    scanf("%f", &pib2);
 
     printf("Números de pontos turísticos: \n");
-    scanf("%d", &pontostur1);
+    scanf("%d", &pontostur2);
 
     //siada carta 1
     printf("carta 1: \n \nEstado: %c \n", estado);
@@ -69,12 +69,12 @@ int main() {
     printf("Pontos turísticos: %d \n \n", pontostur);
 
     //saida carta 2
-    printf("carta 2: \n \nEstado: %c \n", estado1);
-    printf("Codigo da carta: %c%d \n", codigo1, numerocod1);
-    printf("Cidade: %s \n", NomeCidade1);
-    printf("População: %d \n", populacao1);
-    printf("Área: %.3fkm² \n", areaKm1);
-    printf("PIB: %.2f \n", pib1);
+    printf("carta 2: \n \nEstado: %c \n", estado2);
+    printf("Codigo da carta: %c%d \n", codigo2, numerocod2);
+    printf("Cidade: %s \n", NomeCidade2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %.3fkm² \n", areaKm2);
+    printf("PIB: %.2f \n", pib2);
     printf("Pontos turísticos: %d ", pontostur);
 
 
